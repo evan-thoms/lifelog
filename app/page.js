b0e5ca38014ff72af9e3a12f9756d06f7df495fb
evan-thoms <evanthoms@outlook.com>
2024-10-08 13:47:58 -0400

    starting daylog

---
+++ app/page.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import Daylog from '/components/daylog'
+
+export default function Home() {
+  return (
+      <Daylog/>
+  );
+}
---
+++ components/daylog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function daylog() {
+    return (
+        <div>
+            <p>daylog</p>
+        </div>
+    )
+};
+
+export default daylog
